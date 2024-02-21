90a182528e7f129fc9a312eef688138e67573372
hafizhamzaali99 <94824895+hafizhamzaali99@users.noreply.github.com>
2024-02-22 01:32:38 +0500

    home page working

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,9 +1,10 @@
 function selectTab(index) {
-    // Remove 'selected' class from all tabs
-    let tabs = document.querySelectorAll('.tab');
-    console.log(tabs,"tabs")
-    tabs.forEach(tab => tab.classList.remove('selected'));
-  
-    // Add 'selected' class to the clicked tab
-    tabs[index].classList.add('selected');
-  }
+  // Remove 'selected' class from all tabs
+  let tabs = document.querySelectorAll(".tab");
+  console.log(tabs, "tabs");
+  tabs.forEach((tab) => tab.classList.remove("selected"));
+
+  // Add 'selected' class to the clicked tab
+  tabs[index].classList.add("selected");
+}
+
