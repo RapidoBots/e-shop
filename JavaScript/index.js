0990656b467eccc683924bafe0bdfb54962b04f4
hafizhamzaali99 <94824895+hafizhamzaali99@users.noreply.github.com>
2024-02-26 19:35:00 +0500

    product page work

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,3 +1,4 @@
+// function for tab change in home page
 function selectTab(index) {
   // Remove 'selected' class from all tabs
   let tabs = document.querySelectorAll(".tab");
@@ -8,3 +9,19 @@ function selectTab(index) {
   tabs[index].classList.add("selected");
 }
 
+// Function for password toggle in account page
+
+const passwordField = document.getElementById("password");
+const togglePassword = document.querySelector(".password-toggle-icon i");
+
+togglePassword.addEventListener("click", function () {
+  if (passwordField.type === "password") {
+    passwordField.type = "text";
+    togglePassword.classList.remove("fa-eye");
+    togglePassword.classList.add("fa-eye-slash");
+  } else {
+    passwordField.type = "password";
+    togglePassword.classList.remove("fa-eye-slash");
+    togglePassword.classList.add("fa-eye");
+  }
+});
