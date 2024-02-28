086c244d7729ae5c4e48e640a9157b7591dd4e67
hafizhamzaali99 <94824895+hafizhamzaali99@users.noreply.github.com>
2024-02-29 01:07:22 +0500

    home page and header works

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,24 +1,25 @@
 // function for tab change in home page
-function selectTab(index,value) {
+function selectTab(index, value) {
   const tabs = document.querySelectorAll(".tab");
-  const description = document.getElementById("description");
-  const review = document.getElementById("review");
+  const news = document.getElementById("new");
+  const feature = document.getElementById("feature");
+  const top = document.getElementById("top");
   tabs.forEach((tab) => tab.classList.remove("selected"));
   tabs[index].classList.add("selected");
 
-  if(value==="new") {
-    description.style.transform = "translateX(0px)";
-    review.style.transform = "translateX(0px)";
+  if (value === "new") {
+    news.style.display = "block";
+    feature.style.display = "none";
+    top.style.display = "none";
+  } else if (value === "feature") {
+    news.style.display = "none";
+    feature.style.display = "block";
+    top.style.display = "none";
+  } else if (value === "top") {
+    news.style.display = "none";
+    feature.style.display = "none";
+    top.style.display = "block";
   }
-  else if(value==="feature") {
-    description.style.transform = "translateX(1200px)";
-    review.style.transform = "translateX(1200px)";
-  }
-  {
-    description.style.transform = "translateX(1200px)";
-    review.style.transform = "translateX(1200px)";
-  }
-
 }
 
 // Function for password toggle in account page
@@ -38,5 +39,28 @@ togglePassword.addEventListener("click", function () {
   }
 });
 
-// for product tabs
+// for dropdown menu
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
 
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+// handle dropdown 
+function handleDropdownItem(index) {
+  const dropdown_item = document.querySelectorAll(".dropdown-item");
+  dropdown_item.forEach((tab) => tab.classList.remove("selected"));
+  dropdown_item[index].classList.add("selected");
+}
