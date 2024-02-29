1929eee858279bc5b9fdb387ce4506c0edc4f542
hafizhamzaali99 <94824895+hafizhamzaali99@users.noreply.github.com>
2024-03-01 01:06:05 +0500

    homepage card slider

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -22,23 +22,6 @@ function selectTab(index, value) {
   }
 }
 
-// Function for password toggle in account page
-
-const passwordField = document.getElementById("password");
-const togglePassword = document.querySelector(".password-toggle-icon i");
-
-togglePassword.addEventListener("click", function () {
-  if (passwordField.type === "password") {
-    passwordField.type = "text";
-    togglePassword.classList.remove("fa-eye");
-    togglePassword.classList.add("fa-eye-slash");
-  } else {
-    passwordField.type = "password";
-    togglePassword.classList.remove("fa-eye-slash");
-    togglePassword.classList.add("fa-eye");
-  }
-});
-
 // for dropdown menu
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
